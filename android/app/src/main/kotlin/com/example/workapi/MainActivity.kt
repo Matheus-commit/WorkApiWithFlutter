cbb141dc0e690f5da0fbd052b1e88a186892fc50
Matheus Nunes <matheus.nunes@thinklife.com.br>
2023-05-17 11:28:08 -0300

    Commit git

---
+++ android/app/src/main/kotlin/com/example/workapi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.workapi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'apiWork.dart';
+
+void main() {
+  runApp(const MaterialApp(home: Home()));
+}
